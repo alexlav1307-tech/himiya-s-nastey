29acff75986c81e7cc3ffb3b5df2a245539eeab7
alexlav1307-tech <alexlav1307@gmail.com>
2026-08-13 14:17:44 +0300

    Refresh calorie counter cache strategy

--- calorie-counter/sw.js
+++ calorie-counter/sw.js
@@ -1,1 +1,29 @@
-const CACHE='calorie-counter-v2';const ASSETS=['./','./index.html','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='calorie-counter-v3';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil((async()=>{
+    const keys=await caches.keys();
+    await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.mode==='navigate'){
+    event.respondWith(fetch(req).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
+      return res;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(fetch(req).then(res=>{
+    const copy=res.clone();
+    caches.open(CACHE).then(cache=>cache.put(req,copy));
+    return res;
+  }).catch(()=>caches.match(req)));
+});
